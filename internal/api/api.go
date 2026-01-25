642f8271d9e267c16b849a29bc3b165635f3c28f
Toan Pham <ductoan.uit@gmail.com>
2026-01-25 17:37:05 +0000

    feat: add lesson-3

--- internal/api/api.go
+++ internal/api/api.go
@@ -58,6 +58,7 @@ func (a *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 //   - GET /gen-pass: Generates a random password
 //   - GET /health: Health check endpoint
 //   - POST /v1/links/shorten: Shorten URL endpoint
+//   - GET //links/redirect/:code: Redirect URL code endpoint
 //   - GET /swagger/*any: Swagger UI documentation
 func (a *api) RegisterEP() {
 
